bef4f66efa5103c1e978c07076a6ea0ae039eba3
mahmoud <mahmoud@kuwaitnet.dev>
2021-10-07 05:58:34 +0300

    add files

---
+++ human.dart
@@ -0,0 +1,29 @@
+import 'mind.dart';
+import 'senses.dart';
+
+class Human extends Senses {
+  @override
+  Mind hearing() {
+    
+  }
+
+  @override
+  Mind sight() {
+    
+  }
+
+  @override
+  Mind smell() {
+    
+  }
+
+  @override
+  Mind taste() {
+    
+  }
+
+  @override
+  Mind touch() {
+    
+  }
+}
