16a7f71a86962c2ba658adcdee676f3d4f5e7913
mahmoud <mahmoud@kuwaitnet.dev>
2021-10-07 06:04:16 +0300

    fix human file return statement

--- human.dart
+++ human.dart
@@ -4,26 +4,26 @@ import 'senses.dart';
 class Human extends Senses {
   @override
   Mind hearing() {
-    
+    return Mind();
   }
 
   @override
   Mind sight() {
-    
+    return Mind();
   }
 
   @override
   Mind smell() {
-    
+    return Mind();
   }
 
   @override
   Mind taste() {
-    
+    return Mind();
   }
 
   @override
   Mind touch() {
-    
+    return Mind();
   }
 }
