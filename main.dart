f8bb42f0e17ec747845788c3a7473fc747c49f11
mahmoud <mahmoud@kuwaitnet.dev>
2021-10-07 06:22:26 +0300

    add comments

--- main.dart
+++ main.dart
@@ -1,7 +1,9 @@
 import 'human.dart';
 import 'nervous_impulses.dart';
 
+
 void main() {
   Human human = Human();
+ // Senses s = Senses(); this can't be done
   print(human.hearing().nervousImpulsesConverter(NervousImpulses.olfactory));
 }
